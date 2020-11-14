99804fd009360f7147d096cb72eea1a17f0e7447
Zain <reply2zain@gmail.com>
2020-11-14 18:04:37 -0500

    3312g

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ var latestRelease = "Latest Release:\n" +
     "WIP: support for video streams\n" +
     "---3.3.0 introduced---\n" +
     "-Added a new search feature for keys (!k search-starts-with)\n";
-var buildNumber = "3312f";
+var buildNumber = "3312g";
 var servers = {};
 var testingChannelGuildID = 730239813403410619;
 //bot.login(token);
@@ -222,7 +222,7 @@ function playSong(message, whatsp, isMp3) {
                     })
             } else { // video stream
                 let myStream = ytdl(whatsp, {
-                    filter: "audioandvideo",
+                    filter: "videoandaudio",
                     //filter: format => format.container === 'mp4'
                      opusEncoded: false,
                      encoderArgs: ['bass=g=10']
