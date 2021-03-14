34aff651cdea402c6fa405b0ce6d93daf8c81b40
Zain <reply2zain@gmail.com>
2021-03-14 16:42:31 -0400

    minor additional logging

--- index.js
+++ index.js
@@ -1324,7 +1324,7 @@ function playSongToVC(message, whatToPlay) {
 
 process.stdout.on('error', function( err ) {
   if (err.code == "EPIPE" || err.code == "EAGAIN") {
-    
+      console.log("errorz: " + bot.getMaxListeners());
       return;
   }
 });
