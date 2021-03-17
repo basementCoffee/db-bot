1c6ad8f75cdfa5bf2885f87e2609f2bb6e5a1541
brownsycamore <68049698+brownsycamore@users.noreply.github.com>
2021-03-17 05:26:53 -0400

    15th attempt at error fix for alpha build

--- index.js
+++ index.js
@@ -276,11 +276,11 @@ const ytdl = require("ytdl-core-discord");
 
 
 // UPDATE HERE - Before Git Push
-var version = "4.1.3-alpha2.13";
+var version = "4.1.3-alpha2.14";
 var latestRelease =
   "Latest Release (4.1.3-alpha2):\n" +
   "- Third attempt at implementing ytdl-core-discord\n" +
-   "- Last version threw a |SyntaxError: missing ) after argument list| error; 13th attempt at fix \n";
+   "- Last version threw a |SyntaxError: missing ) after argument list| error; 14th attempt at fix \n";
 var servers = {};
 bot.login(process.env.token);
 var whatsp = "";
@@ -1242,15 +1242,8 @@ async function playSongToVC(message, whatToPlay, whatsp) {
       try {
         connection.voice.setSelfDeaf(true);
         
-      let myStream = ytdl(whatsp, {
-        type: 'opus'
-      });
-        
+      let dispatcher = connection.play(await ytdl(whatsp), { type: 'opus' });
       
-      connection.play(await ytdl(myStream), { type: 'opus' });
-      let dispatcher = connection.play(myStream, {
-        type: "opus",
-      });
 
         
         
