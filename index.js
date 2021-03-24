63b531f0cd9da5dc5384967663efcc90c8ace182
Zain <reply2zain@gmail.com>
2021-03-23 20:39:31 -0400

    removed extra comma

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ client2.authorize(function (err, tokens) {
         console.log(err);
     } else {
         console.log("Connected to google apis.");
-        // gsrun(client2, "A", "B", "entries");
     }
 });
 
