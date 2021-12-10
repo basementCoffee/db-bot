dcc3d0e0f32447151ffbc332ec49072e47e6bcf2
Zain Aaban <reply2zain@gmail.com>
2021-12-10 07:22:30 -0500

    fix: disallow undefined positions within move cmd

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -728,12 +728,13 @@ function runMoveItemCommand (message, arr, posA, posB) {
   if (!botInVC(message)) return;
   posA = Math.floor(posA);
   posB = Math.floor(posB);
-  if (!(posA || posB)) message.channel.send(
-    '*two numbers expected: the position of the item to move and it\'s new position*\n`ex: move 1 5`'
-  );
-  else if (arr.length < 3) message.channel.send('*not enough items in the queue*');
-  else if (posA < 1 || posB < 1) {
-    message.channel.send('positions must be greater than 0');
+  const MIN_POS = 1;
+  const MIN_ARR_SIZE = 3;
+  if (!(posA && posB)) message.channel.send(
+    '*two numbers expected: the position of the item to move and it\'s new position*\n`ex: move 1 5`');
+  else if (arr.length < MIN_ARR_SIZE) message.channel.send('*not enough items in the queue*');
+  else if (posA < MIN_POS || posB < MIN_POS) {
+    message.channel.send(`positions must be greater than ${MIN_POS - 1}`);
   } else {
     const item = arr.splice(posA, 1)[0];
     arr.splice(posB, 0, item);
