73ab3d62eda27d7f5f0b5435f9d0887d0a37d8da
Zain Aaban <37006843+Reply2Zain@users.noreply.github.com>
2022-03-23 18:20:16 -0400

    fix: make remove command update embed, refactor

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const {
   resetSession, convertYTFormatToMS, setSeamless, getQueueText, updateActiveEmbed, getHelpList, initializeServer,
   runSearchCommand, runHelpCommand, getTitle, linkFormatter, endStream, unshiftQueue, pushQueue, shuffleQueue,
   createQueueItem, getLinkType, createMemoryEmbed, isAdmin, getAssumption, isCoreAdmin, runMoveItemCommand,
-  insertCommandVerification, convertSeekFormatToSec
+  insertCommandVerification, convertSeekFormatToSec, runRemoveCommand
 } = require('./utils/utils');
 const {
   hasDJPermissions, runDictatorCommand, runDJCommand, voteSystem, clearDJTimer, runResignCommand
@@ -732,22 +732,7 @@ async function runCommandCases (message) {
       break;
     case 'rm':
     case 'remove':
-      if (!message.member.voice?.channel) return message.channel.send('you must be in a voice channel to remove items from the queue');
-      if (server.dictator && message.member.id !== server.dictator.id)
-        return message.channel.send('only the dictator can remove');
-      if (server.voteAdmin.length > 0 && server.voteAdmin.filter(x => x.id === message.member.id).length === 0)
-        return message.channel.send('only a dj can remove');
-      if (server.queue.length < 2) return message.channel.send('*cannot remove from an empty queue*');
-      let rNum = parseInt(args[1]);
-      if (!rNum) {
-        if (server.queue.length === 2) rNum = 1;
-        else return message.channel.send((`Needed a position in the queue to remove (1-${(server.queue.length - 1)})` +
-          `\n***1** is next up in the queue, **${(server.queue.length - 1)}** is the last item in the queue \` Ex: ${prefixString}remove 2\`*`));
-      }
-      if (rNum >= server.queue.length) return message.channel.send('*that position is out of bounds, **' +
-        (server.queue.length - 1) + '** is the last item in the queue.*');
-      server.queue.splice(rNum, 1);
-      message.channel.send('removed item from queue');
+      await runRemoveCommand(message, server, args[1]);
       break;
     case 'move':
       runMoveItemCommand(message, server.queue, args[1], args[2]);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -769,6 +769,33 @@ function runMoveItemCommand (message, arr, posA, posB) {
 }
 
 /**
+ * Removes an item from the queue. Does not allow for the currently playing item to be removed from the queue (index 0).
+ * @param message The message metadata.
+ * @param server The server metadata.
+ * @param itemPosition The position in the queue to remove from (starting from 1).
+ * @return {Promise<*>}
+ */
+async function runRemoveCommand (message, server, itemPosition) {
+  if (!message.member.voice?.channel) return message.channel.send('you must be in a voice channel to remove items from the queue');
+  if (server.dictator && message.member.id !== server.dictator.id)
+    return message.channel.send('only the dictator can remove');
+  if (server.voteAdmin.length > 0 && server.voteAdmin.filter(x => x.id === message.member.id).length === 0)
+    return message.channel.send('only a dj can remove');
+  if (server.queue.length < 2) return message.channel.send('*cannot remove from an empty queue*');
+  let rNum = parseInt(itemPosition);
+  if (!rNum) {
+    if (server.queue.length === 2) rNum = 1;
+    else return message.channel.send((`Needed a position in the queue to remove (1-${(server.queue.length - 1)})` +
+      `\n***1** is next up in the queue, **${(server.queue.length - 1)}** is the last item in the queue \` Ex: remove 2\`*`));
+  }
+  if (rNum >= server.queue.length) return message.channel.send('*that position is out of bounds, **' +
+    (server.queue.length - 1) + '** is the last item in the queue.*');
+  server.queue.splice(rNum, 1);
+  await updateActiveEmbed(server);
+  message.channel.send('*removed item from queue*');
+}
+
+/**
  * Helper for runInsertCommand. Does some preliminary verification.
  * @param message The message object.
  * @param server The server.
@@ -808,5 +835,5 @@ module.exports = {
   resetSession, convertYTFormatToMS, setSeamless, getQueueText, updateActiveEmbed, getHelpList, initializeServer,
   runSearchCommand, runHelpCommand, getTitle, linkFormatter, endStream, unshiftQueue, pushQueue, shuffleQueue,
   createQueueItem, getLinkType, createMemoryEmbed, isAdmin, getTracksWrapper, getAssumption, isCoreAdmin,
-  runMoveItemCommand, destroyBot, insertCommandVerification, convertSeekFormatToSec
+  runMoveItemCommand, destroyBot, insertCommandVerification, convertSeekFormatToSec, runRemoveCommand
 };
