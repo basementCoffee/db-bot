aea3d4405460cd5ec1c33b4a26f35fab7a57aca7
Zain Aaban <37006843+Reply2Zain@users.noreply.github.com>
2022-04-17 05:32:33 -0400

    refactor: optimize seamless utility

--- src/index.js
+++ src/index.js
@@ -1438,11 +1438,6 @@ async function updateVoiceState (update, server) {
       server.leaveVCTimeout = setTimeout(() => {
         server.leaveVCTimeout = null;
         if (update.channel.members.filter(x => !x.user.bot).size < 1) {
-          if (server.seamless.timeout) {
-            clearTimeout(server.seamless.timeout);
-            server.seamless.timeout = null;
-          }
-          server.seamless.function = null;
           update.channel.leave();
         }
       }, leaveVCInt);
