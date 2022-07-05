6ec5e25ed4e15a039d6977c0827d5677a235aa09
Zain Aaban <37006843+Reply2Zain@users.noreply.github.com>
2022-07-05 13:52:41 -0400

    chore: increment build number

--- src/utils/process/BuildNumber.js
+++ src/utils/process/BuildNumber.js
@@ -10,7 +10,7 @@ class BuildNumber {
 
   constructor () {
     this.baseVersionStr = version.split('.').map(x => (x.length < 2 ? `0${x}` : x)).join('');
-    this.extensionStr = '04';
+    this.extensionStr = '05';
     this.buildNoStr = `${this.baseVersionStr}${this.extensionStr}`;
   }
 
