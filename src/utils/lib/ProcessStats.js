5d194b2675dc02f9f41ee068a6fe80702e820989
Zain <reply2zain@gmail.com>
2022-11-12 04:49:08 -0500

    refactor: create LocalServer class for local object

--- src/utils/lib/ProcessStats.js
+++ src/utils/lib/ProcessStats.js
@@ -1,5 +1,5 @@
 const { startupDevMode } = require('./constants');
-const { AudioImpl } = require('./AudioImpl');
+const LocalServer = require('./LocalServer');
 
 // process related statistics
 class ProcessStats {
@@ -102,100 +102,7 @@ class ProcessStats {
    * @param mgid The message guild id.
    */
   initializeServer(mgid) {
-    this.servers.set(mgid, {
-      guildId: mgid,
-      // now playing is the first element
-      queue: [],
-      // newest items are pushed
-      queueHistory: [],
-      /*
-      a log of completed links and their metadata
-      Map<url, {queueItem, numOfPlays: number}>
-       */
-      mapFinishedLinks: new Map(),
-      // continue playing after queue end
-      autoplay: false,
-      // boolean status of looping
-      loop: false,
-      // the number of items sent since embed generation
-      numSinceLastEmbed: 0,
-      // the embed message
-      currentEmbed: undefined,
-      // the collector for the current embed message
-      collector: false,
-      // the playback status message
-      followUpMessage: undefined,
-      // the id of the channel for now-playing embeds
-      currentEmbedChannelId: undefined,
-      // boolean status of verbose mode - save embeds on true
-      verbose: false,
-      // A list of vote admins (members) in a server
-      voteAdmin: [],
-      // the ids of members who voted to skip
-      voteSkipMembersId: [],
-      // the ids of members who voted to rewind
-      voteRewindMembersId: [],
-      // the ids of members who voted to play/pause the link
-      votePlayPauseMembersId: [],
-      // locks the queue for dj mode
-      lockQueue: false,
-      // The member that is the acting dictator
-      dictator: false,
-      // If a start-up message has been sent
-      startUpMessage: false,
-      // the timeout IDs for the bot to leave a VC
-      leaveVCTimeout: false,
-      // the number of consecutive playback errors
-      skipTimes: 0,
-      // a map of user ids to an active query message [id => Message]
-      activeUserQuestion: new Map(),
-      // persistent user settings
-      userSettings: new Map(),
-      // the specific server's audio class
-      audio: new AudioImpl(),
-      // properties pertaining to the active stream
-      streamData: {
-        // the StreamType enum
-        type: null,
-        // the readable stream
-        stream: null,
-        // urlAlt is added if it's a YT stream
-      },
-      // if a twitch notification was sent
-      twitchNotif: {
-        isSent: false,
-        isTimer: false,
-      },
-      // hold a ready-to-go function in case of vc join
-      seamless: {
-        // the name of the type of function
-        function: undefined,
-        // args for the function
-        args: undefined,
-        // optional message to delete
-        message: undefined,
-      },
-      // [id, xdb]
-      // use getXdb2() for user data
-      userKeys: new Map(),
-      // the server's prefix
-      prefix: undefined,
-      // the timeout for the YT search results
-      searchReactionTimeout: undefined,
-      // the timer for the active DJ
-      djTimer: {
-        timer: false,
-        startTime: false,
-        duration: 1800000,
-      },
-      // the last time a DJ tip was sent to a group
-      djMessageDate: false,
-      // error manager - keep track of errors
-      errors: {
-        // if a reaction permissions error occurred
-        permissionReaction: false,
-      },
-    });
+    this.servers.set(mgid, new LocalServer(mgid));
   }
 }
 
