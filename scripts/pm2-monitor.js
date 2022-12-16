408294549c178b1dd1c1dd0c0a311b0e9584be94
Zain <reply2zain@gmail.com>
2022-12-16 18:06:09 -0500

    chore(release): 8.4.2

--- scripts/pm2-monitor.js
+++ scripts/pm2-monitor.js
@@ -8,7 +8,7 @@ const WAIT_TIME = 12000;
 // the maximum number of PM2 restarts allowed
 const MAX_RESTARTS = 5;
 // The last supported version that this project can be safely rolled back to
-const MIN_SUPPORTED_VERSION = '8.4.0';
+const MIN_SUPPORTED_VERSION = '8.4.2';
 
 pm2.connect(async function(err) {
   if (err) {
