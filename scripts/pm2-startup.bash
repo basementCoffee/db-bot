13e2d9521bdba9fa534a255a034d49415eb8bdf8
Zain <reply2zain@gmail.com>
2022-12-18 16:52:27 -0500

    chore: reset pm2 metadata on restart

--- scripts/pm2-startup.bash
+++ scripts/pm2-startup.bash
@@ -4,7 +4,8 @@ echo running pm2 script...
 NAME=${1:-vibe}
 
 if pm2 restart $NAME ; then
-  echo started
+  pm2 reset $NAME
+  echo restarted $NAME
 else
   pm2 start ./src/index.js --name $NAME
 fi
