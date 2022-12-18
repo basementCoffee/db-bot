7e253685f42f087c8f623b193837270b402d9365
Zain <reply2zain@gmail.com>
2022-12-18 16:05:50 -0500

    chore: improve update-script

--- scripts/pm2-monitor.js
+++ scripts/pm2-monitor.js
@@ -2,13 +2,57 @@
 const pm2 = require('pm2');
 const { exec } = require('child_process');
 const name = process.argv[2] || 'vibe';
+const fs = require('fs');
 
 // wait time before checking PM2's status
 const WAIT_TIME = 12000;
 // the maximum number of PM2 restarts allowed
 const MAX_RESTARTS = 5;
 // The last supported version that this project can be safely rolled back to
-const MIN_SUPPORTED_VERSION = '8.4.3';
+let minSupportedVersion = '8.4.3';
+// path of where the minSupportedVersion is stored (to be stored outside the git project)
+const FILE_PATH_MIN_VERSION = './.min_version_vibe.txt';
+
+fs.readFile(FILE_PATH_MIN_VERSION, (err, contents) => {
+  console.log(contents.toString());
+  const minVersionInFile = contents.toString();
+  if (minVersionInFile && minVersionInFile.includes('.')) {
+    const diff = compareTwoVersions(minSupportedVersion, minVersionInFile);
+    if (diff <= 0) {
+      // if the local variable has a smaller/same version
+      minSupportedVersion = minVersionInFile;
+      return;
+    }
+  }
+  // if the local variable has a larger version
+  fs.writeFile(FILE_PATH_MIN_VERSION, minSupportedVersion, (e) => {console.log(e);});
+});
+
+// returns a negative if versionA is smaller, 0 if they are equal, positive if version A is larger
+function compareTwoVersions(versionA, versionB) {
+  if (!versionA || !versionB) {
+    console.error('error: mission version');
+    return 0;
+  }
+  const versionArr1 = versionA.split('.').map((x) => Number(x));
+  const versionArr2 = versionB.split('.').map((x) => Number(x));
+  const smallerArraySize = versionArr1.length < versionArr2.length ? versionArr1.length : versionArr2.length;
+  if (versionArr1.length !== versionArr2.length) {
+    console.error('error: versions are incompatible');
+  }
+  const determineDifference = (num = 0) => {
+    if (num > smallerArraySize) return 0;
+    // positive when current version section is larger than supported version section
+    const difference = versionArr1[num] - versionArr2[num];
+    if (difference === 0) {
+      return determineDifference(num + 1);
+    }
+    else {
+      return difference;
+    }
+  };
+  return determineDifference();
+}
 
 pm2.connect(async function(err) {
   if (err) {
@@ -24,26 +68,8 @@ pm2.connect(async function(err) {
     processes.forEach(function(process) {
       if (process.pm2_env.restart_time > MAX_RESTARTS) {
         console.log(`[ERROR] process is in bad standing {restarts: ${process.pm2_env.restart_time}}`);
-        // The current program version in this project [major | minor | patch]
-        const currentVersionArr = process.pm2_env.version.split('.').map((x) => Number(x));
-        // The last supported version [major | minor | patch]
-        const supportedVersionArr = MIN_SUPPORTED_VERSION.split('.').map((x) => Number(x));
-        const isOutdated = (num) => {
-          if (!currentVersionArr[num]) return false;
-          // positive when current version section is larger than supported version section
-          const difference = currentVersionArr[num] - supportedVersionArr[num];
-          if (difference < 0) {
-            return true;
-          }
-          else if (difference === 0) {
-            return isOutdated(num + 1);
-          }
-          else {return false;}
-        };
-        if (isOutdated(0)) {
-          console.log('version too old to rollback');
-        }
-        else {
+        const res = compareTwoVersions(process.pm2_env.version, minSupportedVersion);
+        if (res > 0) {
           console.log('attempting rollback...');
           const fileName = 'getCommitIds.bash';
           exec(`bash scripts/${fileName}`, (err, commitId, stderr) => {
@@ -56,6 +82,9 @@ pm2.connect(async function(err) {
               }
             });
           });
+        }
+        else {
+          console.log('version too old to rollback');
         }
       }
       else {
