547090ea08ca3d765a881ced495349e23d57080c
Zain <reply2zain@gmail.com>
2022-12-20 03:17:02 -0500

    chore: update install scripts, add ytsr

--- scripts/postNodeInstallScript.js
+++ scripts/postNodeInstallScript.js
@@ -10,19 +10,29 @@ function replaceContents(file, replacement, cb) {
   });
 }
 
-
-replaceContents('./node_modules/ytdl-core/lib/sig.js', './scripts/ytdlCoreSig.txt', err => {
+const fileReplacementCallback = (err, name) => {
   if (err) {
+    console.log(`[ERROR] could not load ${name}`);
     // handle errors here
     throw err;
   }
-  console.log('[loaded updated ytdl-core scripts]');
-});
-
-replaceContents('./node_modules/spotify-url-info/src/index.js', './scripts/spotify-url-info-index.txt', err => {
-  if (err) {
-    // handle errors here
-    throw err;
+  else {
+    console.log(`[SUCCESS] loaded updated ${name}`);
   }
-  console.log('[loaded updated spotify-url-info scripts]');
-});
+};
+
+
+replaceContents('./node_modules/ytdl-core/lib/sig.js', './scripts/textFileAssets/ytdlCoreSig.txt',
+  (err) => {
+    fileReplacementCallback(err, 'ytdl-core scripts');
+  });
+
+replaceContents('./node_modules/spotify-url-info/src/index.js', './scripts/textFileAssets/spotify-url-info-index.txt',
+  (err) => {
+    fileReplacementCallback(err, 'spotify-url-info scripts');
+  });
+
+replaceContents('./node_modules/ytsr/lib/parseItem.js', './scripts/textFileAssets/ytsr-parseItem.txt',
+  (err) => {
+    fileReplacementCallback(err, 'ytsr scripts');
+  });
