5d8a3c39192929d1f860a3cdbc55fd7964abb94a
Zain <reply2zain@gmail.com>
2023-01-14 18:10:26 -0500

    refactor: update script file paths

--- scripts/pm2-startup.bash
+++ scripts/pm2-startup.bash
@@ -7,7 +7,7 @@ if pm2 restart $NAME ; then
   pm2 reset $NAME
   echo restarted $NAME
 else
-  pm2 start ./src/index.js --name $NAME
+  pm2 start ./dist/index.js --name $NAME
 fi
 
 node ./scripts/pm2-monitor.js $NAME
