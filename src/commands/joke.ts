a331e4b08d72bbad39d18c06a9e6dddf3913cf4e
Zain <reply2zain@gmail.com>
2023-01-14 15:42:43 -0500

    refactor: update import for joke cmd

--- src/commands/joke.ts
+++ src/commands/joke.ts
@@ -1,15 +1,15 @@
-const cheerio = require('cheerio');
+import { load } from 'cheerio';
 
 /**
  * Gets a random joke from the internet.
  * @returns {Promise<string>} The joke.
  */
 function getJoke() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fetch('https://icanhazdadjoke.com/').then((response) => {
       response.text().then((htmlText) => {
         // parse html
-        const $ = cheerio.load(htmlText);
+        const $ = load(htmlText);
         let val = $('p').text();
         val = val.substring(0, val.indexOf('icanhazdadjoke.com'));
         resolve(val);
