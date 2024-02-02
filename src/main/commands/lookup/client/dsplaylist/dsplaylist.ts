43268504b9498bf08f02dfacacfd136bdc15c268
Zain <reply2zain@gmail.com>
2024-02-02 18:27:12 -0500

    refactor: remove debug logs

--- src/main/commands/lookup/client/dsplaylist/dsplaylist.ts
+++ src/main/commands/lookup/client/dsplaylist/dsplaylist.ts
@@ -1,13 +1,11 @@
-import { MessageEventLocal } from "../../../../utils/lib/types";
-import commandHandlerCommon from "../../../CommandHandlerCommon";
-import { getSheetName, isShortCommandNoArgs } from "../../../../utils/utils";
+import { MessageEventLocal } from '../../../../utils/lib/types';
+import commandHandlerCommon from '../../../CommandHandlerCommon';
+import { getSheetName, isShortCommandNoArgs } from '../../../../utils/utils';
 
 exports.run = async (event: MessageEventLocal) => {
   const message = event.message;
   const server = event.server;
-  console.log("A");
   if (isShortCommandNoArgs(event.args, message.guild!, event.statement)) return;
-  console.log("B");
   commandHandlerCommon
     .playDBPlaylist(event.args, message, getSheetName(message.member!.id), false, true, server, true)
     .then();
