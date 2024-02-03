bb056d7ea3671d4daa645aa2e3863e1a198401b8
Zain <reply2zain@gmail.com>
2024-02-03 18:59:13 -0500

    refactor: replace node fetch w/ isomorphic-unfetch

--- src/main/commands/joke.ts
+++ src/main/commands/joke.ts
@@ -1,13 +1,14 @@
 import { load } from 'cheerio';
+import fetch from 'isomorphic-unfetch';
 
 /**
  * Gets a random joke from the internet.
  * @returns {Promise<string>} The joke.
  */
-function getJoke() {
+function getJoke(): Promise<string> {
   return new Promise((resolve) => {
-    fetch('https://icanhazdadjoke.com/').then((response) => {
-      response.text().then((htmlText) => {
+    fetch('https://icanhazdadjoke.com/').then((response: any) => {
+      response.text().then((htmlText: string) => {
         // parse html
         const $ = load(htmlText);
         let val = $('p').text();
